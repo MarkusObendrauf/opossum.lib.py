2784b4f786925072da47eea38d0dc2dfc1427b3e
Markus Obendrauf <markus.obendrauf@tngtech.com>
2023-05-26 14:03:26 +0200

    test commit

--- src/opossum_lib/file_generation.py
+++ src/opossum_lib/file_generation.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2023 TNG Technology Consulting GmbH <https://www.tngtech.com>
 #
 # SPDX-License-Identifier: Apache-2.0
+
 import json
 from dataclasses import fields
 from pathlib import Path
